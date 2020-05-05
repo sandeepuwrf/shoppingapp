4dbdca2a242898279eb88c6302a1ec10e3ccb98b
sandeepuwrf <57881234+sandeepuwrf@users.noreply.github.com>
2020-05-05 15:35:40 -0500

    US: Order - Show Details
    Order Screen - Show Details or Hide details. Used react hooks STATE here.

--- components/shop/CartItem.js
+++ components/shop/CartItem.js
@@ -11,7 +11,7 @@ const CartItem = props => {
             </View>
             <View style={styles.itemData}>
                 <Text style={styles.mainText}>${props.amount.toFixed(2)}</Text>
-                {<TouchableOpacity
+                {props.deletable && <TouchableOpacity
                     onPress={props.onRemove} style={styles.deleteButton}>
                     <Ionicons
                         name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'}
--- components/shop/OrderItem.js
+++ components/shop/OrderItem.js
@@ -1,40 +1,49 @@
-import React from 'react';
-import {View,Text,Button,StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import CartItem from './CartItem';
 import Colors from '../../constants/Colors';
 
 const OrderItem = props => {
-    console.log("Date: ", + props.date )
+    const [showDetails, setShowDetails] = useState(false);
+
     return (
-    <View style={styles.orderItem}>
-        <View style={styles.summary}>
-            <Text style={styles.totalAmount}>${props.amount.toFixed(2)}</Text>
-            
-            <Text style={styles.date}>{props.date}</Text>
+        <View style={styles.orderItem}>
+            <View style={styles.summary}>
+                <Text style={styles.totalAmount}>${props.amount.toFixed(2)}</Text>
+
+                <Text style={styles.date}>{props.date}</Text>
+            </View>
+            <Button color={Colors.primary} title={!showDetails ? "Show Details" : "Hide Details"} onPress={() => {
+                setShowDetails(prevState => !prevState);
+            }} />
+            {showDetails && <View style={styles.detailItems}>
+                {props.items.map(cartItem => <CartItem
+                    key={cartItem.productId}
+                    quantity={cartItem.quantity} amount={cartItem.sum} title={cartItem.productTitle} />)}
+            </View>
+            }
         </View>
-        <Button color={Colors.primary} title="Show Details"/>
-    </View>
     )
 };
 
 const styles = StyleSheet.create({
-    orderItem:{
+    orderItem: {
         shadowColor: 'black',
         shadowOpacity: 0.26,
-        shadowOffset: {width:0, height:2},
+        shadowOffset: { width: 0, height: 2 },
         shadowRadius: 8,
         elevation: 5,
         borderRadius: 10,
         backgroundColor: 'white',
-        margin:20,
-        padding:10,
+        margin: 20,
+        padding: 10,
         alignItems: 'center'
 
     },
     summary: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems:'center',
+        alignItems: 'center',
         width: '100%',
         margin: 15
 
@@ -44,6 +53,9 @@ const styles = StyleSheet.create({
     },
     date: {
         fontSize: 16
+    },
+    detailItems: {
+        width:'100%'
     }
 
 });
